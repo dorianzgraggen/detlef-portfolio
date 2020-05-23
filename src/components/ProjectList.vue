7925af5071e921c5a5dded56ef8a5c06e4406fba
dorianzgraggen <dorianfx1@gmail.com>
2020-05-23 15:59:55 +0200

    Put the project list in a single file component

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+  
+     <div class="portfolio-item" v-for="(post, index) in array" :key="post.id" :class="'project-' + (((index + parseInt(startIndex, 10)) % 2))">
+        
+        <div class="desc-container">
+          <g-link :to="post.node.path">
+            <h4>{{ post.node.title }}</h4>
+          </g-link>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit repellat iure officiis mollitia tempore magnam delectus sequi praesentium dolores dolorem, a nemo deleniti laborum at temporibus odio, inventore libero deserunt.</p>
+
+        </div>
+        
+        <div class="thumb-container">
+          <g-image src="/uploads/thumb_placeholder.png"></g-image>
+          <!-- <g-image :src="post.node.featuredImage" width=""></g-image> -->
+        </div>
+      </div>
+    
+  </div>
+
+
+
+
+
+</template>
+
+
+<script>
+export default {
+  name: 'ProjectList',
+  props: ['array', "startIndex"],
+}
+</script>
+
+
+
+<style>
+
+.portfolio-item {
+  display: flex;
+  margin-top: 36px;
+  margin-bottom: 72px;
+}
+
+.desc-container {
+  width: 50%;
+}
+
+.thumb-container {
+  width: 50%;
+}
+.thumb-container .g-image {
+  width: 80%;
+}
+
+.project-1 {
+  flex-direction: row-reverse;
+}
+
+.project-0 .g-image {
+  float: right;
+}
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,62 +4,42 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    <h1>Hello, world! </h1>
+   
+
+
+    <h1>Hello, world! </h1>
 
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
 
     <h2>Projects</h2>
-    <ul>
-      <li v-for="post in $page.posts.edges" :key="post.id">
+    <!-- <ul>
+      <li v-for="(post, index) in $page.posts.edges" :key="post.id">
         <g-link :to="post.node.path">
-          {{ post.node.title }}
+          {{ post.node.title }} {{ index }}
         <img alt="Example image" v-bind:src="post.node.featuredImage">
 
         </g-link>
 
       </li>
-    </ul>
+    </ul> -->
 
     <h3>cgbookcase.com</h3>
-     <ul>
-      <li v-for="post in $page.posts.edges" :key="post.id" v-if="post.node.category == 'cgbookcase'">
-        <g-link :to="post.node.path">
-          {{ post.node.title }}
-        </g-link>
-      </li>
-    </ul>
-
+    <ProjectList :array="$page.cgbPosts.edges" :startIndex="0"/>
     
     <h3>3D Renderings</h3>
-     <ul>
-      <li v-for="post in $page.posts.edges" :key="post.id" v-if="post.node.category == '3d'">
-        <g-link :to="post.node.path">
-          {{ post.node.title }}
-        </g-link>
-      </li>
-    </ul>
-
+    <ProjectList :array="$page.renderingPosts.edges" :startIndex="$page.cgbPosts.edges.length"/>
     
     <h3>Virtual Reality</h3>
-     <ul>
-      <li v-for="post in $page.posts.edges" :key="post.id" v-if="post.node.category == 'vr'">
-        <g-link :to="post.node.path">
-          {{ post.node.title }}
-        </g-link>
-      </li>
-    </ul>
-
+    <ProjectList :array="$page.vrPosts.edges" :startIndex="$page.cgbPosts.edges.length + $page.renderingPosts.edges.length"/>
     
     <h3>More</h3>
-     <ul>
-      <li v-for="post in $page.posts.edges" :key="post.id" v-if="post.node.category == 'other'">
-        <g-link :to="post.node.path">
-          {{ post.node.title }}
-        </g-link>
-      </li>
-    </ul>
+    <ProjectList :array="$page.morePosts.edges" :startIndex="$page.cgbPosts.edges.length + $page.renderingPosts.edges.length + $page.vrPosts.edges.length"/>
+
+    
+
 
 
 
@@ -85,22 +65,85 @@ query ProjectPosts {
         featuredImage
       }
     }
+  },
+  cgbPosts: allProjectPost(filter: {category: {eq: "cgbookcase"}}) {
+    edges {
+      node {
+        title
+        slug
+        content
+        category
+        path
+        featuredImage
+      }
+    }
+  },
+  renderingPosts: allProjectPost(filter: {category: {eq: "3d"}}) {
+    edges {
+      node {
+        title
+        slug
+        content
+        category
+        path
+        featuredImage
+      }
+    }
+  },
+  vrPosts: allProjectPost(filter: {category: {eq: "vr"}}) {
+    edges {
+      node {
+        title
+        slug
+        content
+        category
+        path
+        featuredImage
+      }
+    }
+  },
+  morePosts: allProjectPost(filter: {category: {eq: "more"}}) {
+    edges {
+      node {
+        title
+        slug
+        content
+        category
+        path
+        featuredImage
+      }
+    }
   }
+
 }
 </page-query>
 
 
 
+
 <script>
+import ProjectList from '~/components/ProjectList.vue'
+
 export default {
   metaInfo: {
     title: 'Hello, world!'
+  },
+  components: {
+    ProjectList
   }
 }
+
+
+
 </script>
 
 <style>
 .home-links a {
   margin-right: 1rem;
 }
+
+h3 {
+  text-align: center;
+}
+
 </style>
