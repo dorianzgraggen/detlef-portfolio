6c1ac385b42cc48c14b96bb958ada1720e99d2de
dorianzgraggen <dorianfx1@gmail.com>
2020-06-07 14:55:32 +0200

    make cube functional

--- src/components/ThreeScene.vue
+++ src/components/ThreeScene.vue
@@ -10,6 +10,7 @@
 // import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js';
 var THREE = require("three");
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default {
   name: 'ThreeScene',
@@ -43,20 +44,30 @@ export default {
     camera.rotation.y =  THREE.Math.degToRad( 21 )
     // camera.lookAt(0, 0, 0)
 
+    var controls = new OrbitControls( camera, renderer.domElement );
+    controls.update();
+
     const scene = new THREE.Scene();
 
     var loader = new GLTFLoader();
 
 
-    // {
-    //     const color = 0xFFFFFF;
-    //     const intensity = 1;
-    //     const light = new THREE.DirectionalLight(color, intensity);
-    //     light.position.set(-1, 2, 4);
-    //     scene.add(light);
-    // }
     {
-        var light = new THREE.AmbientLight( 0xffffff ); // soft white light
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+     {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(1, 2, -4);
+        scene.add(light);
+    }
+    {
+        var light = new THREE.AmbientLight( 0xffffff, 0.1 ); // soft white light
         scene.add( light );
     }
 
@@ -85,7 +96,8 @@ export default {
 
     console.log("loading eeeeh")
 
-    let cublets = [];
+    // let cublets = [];
+    let allCublets = [];
 
     var group = new THREE.Group();
     loader.load(
@@ -97,11 +109,14 @@ export default {
             group.add( gltf.scene );
 
             gltf.animations; // Array<THREE.AnimationClip>
-            cublets = gltf.scene; // THREE.Group
+            gltf.scene; // THREE.Group
             gltf.scenes; // Array<THREE.Group>
             gltf.cameras; // Array<THREE.Camera>
             gltf.asset; // Object
 
+            gltf.scene.children.forEach(element => {
+                allCublets.push(element)
+            });
         },
         // called while loading is progressing
         function ( xhr ) {
@@ -118,26 +133,174 @@ export default {
         
     );
     scene.add( group );
+    console.log(group)
+ 
+
+
+    let rotationGroup = new THREE.Group();
+    group.add(rotationGroup);
+
+
+    // Define steps
+
+    let sides = [
+        "front",
+        "back",
+        "left",
+        "right",
+        "up",
+        "down",
+    ]
+
+
+    let axes = [
+        "z", "z",
+        "x", "x",
+        "y", "y"
+    ]
+    let directions = [
+        -90,
+        -90,
+        90,
+        90
+    ]
+
+    let steps = [];
+
+
+    for (let i = 0; i < 10; i++) {
+        let side = Math.floor(Math.random() * 6);
+        let direction = Math.floor(Math.random() * 4);
+        let step = {
+            side: side,
+            direction: direction
+        }
+        steps.push(step)
+    }
+    console.log(steps)
 
 
     // Keybindings
     document.addEventListener('keydown', handleKeys);
 
+    let currentStep = 0;
+    let bMoveGroupBack = false;
+    let moveBackCublets = [];
+
     function handleKeys(e) {
+
         if (e.key == "d") {
             console.log("");
-            if (cublets.children) {
+            if (allCublets != []) {
             // console.log(cublets.children.length)
         
             // cublets.children[1].position.y += 0.01;
-                cublets.children.forEach(cublet => {
-                    if (Math.round(cublet.position.y) == -1) {
-                        cublet.position.y -= 0.01;
+                allCublets.forEach(cublet => {
+                    if (Math.round(cublet.position["y"]) == -1) {
+                        cublet.position.y -= 0.02;
                     }
                 });
             } else {
-                console.log("cublets.children does not exist");
+                console.log("allCublets is empty");
             }
+        }
+
+         if (e.key == "l") {
+             console.log(allCublets)
+         }
+
+        if (e.key == "n") {
+            console.log(group.children)
+            let direction = directions[steps[currentStep].direction];
+    	    let axis = axes[steps[currentStep].side]
+            
+    
+            
+            let selectedCublets = [];
+            console.log(selectedCublets)
+
+            
+            console.log("=============")
+            console.log(allCublets)
+            console.log("=============")
+
+            allCublets.forEach(cublet => {
+                let conditions = [
+                    (Math.round(cublet.position["z"]) == -1),
+                    (Math.round(cublet.position["z"]) == 1), 
+                    (Math.round(cublet.position["x"]) == -1),
+                    (Math.round(cublet.position["x"]) == 1),
+                    (Math.round(cublet.position["y"]) == 1),
+                    (Math.round(cublet.position["y"]) == -1),  
+                ];
+
+                
+                // console.log(cublet.position[axis])
+                
+                if (conditions[steps[currentStep].side]) {
+
+                    console.log(cublet.name)
+                    console.log(axis)
+
+                    selectedCublets.push(cublet);
+                    rotationGroup.attach(cublet);
+                }
+                
+            });
+            console.log("added")
+            console.log(selectedCublets)
+
+
+            console.log("attached")
+            console.log(rotationGroup)
+
+
+
+            rotationGroup.rotation[axis] += THREE.Math.degToRad(direction);
+            // rotationGroup.position.y += -.5;
+            selectedCublets.forEach(cublet => {
+                group.attach(cublet)
+            });
+
+
+            rotationGroup.rotation[axis] -= THREE.Math.degToRad(direction);
+            console.log("re-attached")
+            console.log(rotationGroup)
+            
+
+
+            currentStep++;
+        }
+
+        if (e.key == "s") {
+            currentStep--;
+            console.log(currentStep);
+
+            allCublets.forEach(cublet => {
+                let conditions = [
+                    (Math.round(cublet.position["z"]) == -1),
+                    (Math.round(cublet.position["z"]) == 1), 
+                    (Math.round(cublet.position["x"]) == -1),
+                    (Math.round(cublet.position["x"]) == 1),
+                    (Math.round(cublet.position["y"]) == 1),
+                    (Math.round(cublet.position["y"]) == -1),  
+                ];
+
+                
+                // console.log(cublet.position[axis])
+                
+                if (conditions[steps[currentStep].side]) {
+
+                    console.log(cublet.name)
+                    //console.log(axis)
+
+                    moveBackCublets.push(cublet);
+                    rotationGroup.attach(cublet);
+                }
+                
+            });
+
+            bMoveGroupBack = true;
         }
     }
 
@@ -152,6 +315,8 @@ export default {
         return needResize;
     }
 
+    let moveBackStep = 1;
+
     let then = 0;
     let delta = 0;
     function render(time) {
@@ -159,16 +324,7 @@ export default {
         delta = time - then; 
         then = time;
 
-        if (cublets.children) {
-            // console.log(cublets.children.length)
         
-            // cublets.children[1].position.y += 0.01;
-
-        
-            // console.log(delta)
-            // console.log("FPS:", 1 / delta)
-            group.rotation.y += THREE.Math.degToRad(1);
-        }
 
 
         if (resizeRendererToDisplaySize(renderer)) {
@@ -187,6 +343,30 @@ export default {
         cube.rotation.x = rot;
         cube.rotation.y = rot;
         });
+
+        if (bMoveGroupBack) {
+            let direction = directions[steps[currentStep].direction];
+    	    let axis = axes[steps[currentStep].side]
+
+            rotationGroup.rotation[axis] -= THREE.Math.degToRad(direction) / 10;   
+
+            
+            if (moveBackStep >= 10) {
+                console.log("ended")
+                bMoveGroupBack = false;
+                moveBackStep = 1;
+                moveBackCublets.forEach(cublet => {
+                    group.attach(cublet)
+                });
+                moveBackCublets = [];
+                rotationGroup.rotation[axis] += THREE.Math.degToRad(direction);
+            } else {
+                moveBackStep++;
+            }
+
+        }
+
+        controls.update();
 
         renderer.render(scene, camera);
 
